b79ec491f9c8154d1aa60f52090ba97af111f25c
Real <real.h.bakhit.17@aberdeen.ac.uk>
2018-02-21 17:56:38 +0000

    Calculating craft per head of population ration in welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,15 @@
 class WelcomeController < ApplicationController
   def index
+    @ratio=Hash.new
+    counter = 0
+    states=State.all
+    states.each do |s|
+      s.breweries.each do |b|
+        b.beers.each do
+          counter += 1
+        end
+      end
+      @ratio[s.abrv]=(counter/(s.population/100000).to_f).to_f
+    end
   end
 end
